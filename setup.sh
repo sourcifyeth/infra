fc640ff707273d1b5d8f955d0f11ddebd988a4ed
Rafael Matias <rafael@skyle.net>
2023-08-23 18:04:51 +0200

    add some tooling and ci

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Plugin list
+asdf plugin add age https://github.com/threkk/asdf-age.git
+asdf plugin add shellcheck https://github.com/luizm/asdf-shellcheck.git
+asdf plugin add sops https://github.com/feniix/asdf-sops.git
+asdf plugin-add helm https://github.com/Antiarchitect/asdf-helm.git
+
+asdf install
