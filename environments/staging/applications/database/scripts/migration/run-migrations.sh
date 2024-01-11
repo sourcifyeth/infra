7b0bcad5626e4d1417336193f290c02baaaacf96
Rafael Matias <rafael@skyle.net>
2024-01-11 16:30:48 +0100

    staging: add async pkg for migration script

--- environments/staging/applications/database/scripts/migration/run-migrations.sh
+++ environments/staging/applications/database/scripts/migration/run-migrations.sh
@@ -5,6 +5,7 @@ git checkout 195f6874a42e3022ca65dbff38ddadbbae191897
 cd services/database
 sed -i "s/localhost/$POSTGRES_HOST/g" database.json
 npm install
+npm install async
 
 while ! </dev/tcp/$POSTGRES_HOST/$POSTGRES_PORT; do
   echo "$POSTGRES_HOST:$POSTGRES_PORT isn't up. waiting ..."
