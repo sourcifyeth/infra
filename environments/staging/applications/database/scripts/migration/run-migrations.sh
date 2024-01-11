9a6e469debab1cb9f8320810d6af4a0da96722d5
Rafael Matias <rafael@skyle.net>
2024-01-11 16:34:24 +0100

    staging: update migrations branch

--- environments/staging/applications/database/scripts/migration/run-migrations.sh
+++ environments/staging/applications/database/scripts/migration/run-migrations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 git clone https://github.com/ethereum/sourcify.git
 cd sourcify
-git checkout 195f6874a42e3022ca65dbff38ddadbbae191897
+git checkout sourcify-database
 cd services/database
 sed -i "s/localhost/$POSTGRES_HOST/g" database.json
 npm install
