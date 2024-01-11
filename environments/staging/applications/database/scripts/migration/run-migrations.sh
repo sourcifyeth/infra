58a16dc0bf7e67d6dd41deb9b76da3defa932bbd
Rafael Matias <rafael@skyle.net>
2024-01-11 16:14:29 +0100

    staging: try adding the new db

---
+++ environments/staging/applications/database/scripts/migration/run-migrations.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -xe
+git clone https://github.com/ethereum/sourcify.git
+cd sourcify
+git checkout 195f6874a42e3022ca65dbff38ddadbbae191897
+cd services/database
+sed -i "s/localhost/$POSTGRES_HOST/g" database.json
+npm install
+
+while ! </dev/tcp/$POSTGRES_HOST/$POSTGRES_PORT; do
+  echo "$POSTGRES_HOST:$POSTGRES_PORT isn't up. waiting ..."
+  sleep 5;
+done;
+
+npm run migrate:up
