37eae5a04edc15f30e10157ab69e21487575c17d
Rafael Matias <rafael@skyle.net>
2024-06-05 18:01:56 +0200

    staging: reenable db v2

---
+++ environments/staging/applications/database-v2/scripts/migration/simple-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+git clone https://github.com/ethereum/sourcify.git
+cd sourcify
+git checkout staging
+cd services/database
+sed -i "s/localhost/$POSTGRES_HOST/g" database.json
+npm install
+npm install async
+
+while ! </dev/tcp/$POSTGRES_HOST/$POSTGRES_PORT; do
+  echo "$POSTGRES_HOST:$POSTGRES_PORT isn't up. waiting ..."
+  sleep 5;
+done;
